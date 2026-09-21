9a201600fd30dc526186678375616addce781145
Lancelot Grafilo <olifarg12345@gmail.com>
2026-09-21 14:24:11 +0800

    feat: containerize api and add to docker compose

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -4,8 +4,8 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.POSTGRES_PORT) || 5432,
+  host: process.env.DB_HOST || 'localhost', 
+  port: Number(process.env.DB_PORT || process.env.POSTGRES_PORT) || 5432,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
